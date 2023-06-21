206771e2134dd662605fee324561305a1c18c339
Akuya Ekorot <akuyaekorot@gmail.com>
2023-06-21 16:41:37 +0300

    amenity

--- main_place_amenities.py
+++ main_place_amenities.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 """ Test link Many-To-Many Place <> Amenity
 """
-from models import *
+from models.state import State
+from models.city import City
+from models.user import User
+from models.place import Place
+from models.amenity import Amenity
+from models import storage
 
 # creation of a State
 state = State(name="California")
@@ -12,7 +17,7 @@ city = City(state_id=state.id, name="San Francisco")
 city.save()
 
 # creation of a User
-user = User(email="john@snow.com", password="johnpwd")
+user = User(first_name="John", last_name="Snow", email="john@snow.com", password="johnpwd")
 user.save()
 
 # creation of 2 Places
--- models/amenity.py
+++ models/amenity.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Integer, Float, ForeignKey
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models.place import place_amenity
+
 
 class Amenity(BaseModel, Base):
     __tablename__ = 'amenities'
